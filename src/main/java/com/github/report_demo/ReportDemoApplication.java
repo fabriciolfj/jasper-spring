12358964101669b56664bbf8b0f84630948bd223
Fabricio Jacob <fjacob@boltenergy.com.br>
2024-06-30 20:44:15 -0300

    init project

---
+++ src/main/java/com/github/report_demo/ReportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.report_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportDemoApplication.class, args);
+	}
+
+}
